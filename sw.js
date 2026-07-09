cd5eb52918ef8970fa3a0cb5f1d623b23174e2bb
ramadanrabi549-sys <ramadanrabi549@gmail.com>
2026-07-09 20:48:03 +0200

    Implement service worker caching and activation logic

--- sw.js
+++ sw.js
@@ -1,6 +1,41 @@
-self.addEventListener('install', function(event) {
-  console.log('تم تثبيت Service Worker');
+const CACHE_NAME = 'dalil-musafer-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// لما التطبيق يتثبت، خزن الملفات دي
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Service Worker: Caching files');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+
+// لما المستخدم يفتح صفحة، هاتها من الكاش الأول
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// امسح الكاش القديم لما نحدث
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) {
+          console.log('Service Worker: Removing old cache');
+          return caches.delete(key);
+        }
+      })
+    ))
+  );
 });
